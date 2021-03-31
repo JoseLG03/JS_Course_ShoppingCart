843fb8e48c5875672b6287c322b56bc9af53322f
JoseLG03 <antares069@gmail.com>
2021-03-30 20:28:23 -0600

    add event listener

---
+++ js/app.js
@@ -0,0 +1,30 @@
+const cart = document.querySelector("#carrito");
+
+const container = document.querySelector("#lista-carrito tbody");
+
+const btnCalcel = document.querySelector("#vaciar-carrito");
+
+const coursesList = document.querySelector("#lista-cursos");
+
+const btnAdd = document.querySelector("#agregar-carrito");
+
+//Load listeners
+
+loadEventsListeners();
+
+function loadEventsListeners(){
+    //function add course when click btn add 
+    coursesList.addEventListener("click", addCourse);
+};
+
+//functions
+
+function addCourse(e){
+    
+    e.preventDefault();
+
+    if(e.target.classList.contains("agregar-carrito")){
+        console.log("Agregado");    
+    }
+    
+};
